8247c9ce98eb4efc8ab155b3ec3b28b31d37d010
Benoit JAFFUEL <benoit.jaffuel@edu.devinci.fr>
2021-11-03 18:21:24 +0100

    Ajoutez des fichiers projet.

---
+++ Models/RepositoryPerson.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq001.Models
+{
+    public class RepositoryPerson
+    { //< ou >
+        public static List<Person> persons()
+        {
+            List<Person> people = new List<Person>()
+            {
+                new Person() {ID =1, Name = "Person 1", Adress = "Country 1", Salary = 10000},
+                new Person() {ID =2, Name = "Person 12", Adress = "Country 1", Salary = 20000},
+                new Person() {ID =3, Name = "Person 123", Adress = "Country 1", Salary = 30000},
+                new Person() {ID =4, Name = "Person 1234", Adress = "Country 1", Salary = 40000},
+                new Person() {ID =5, Name = "Person 12345", Adress = "Country 1", Salary = 50000},
+                new Person() {ID =6, Name = "Person 123456", Adress = "Country 1", Salary = 60000},
+                new Person() {ID =7, Name = "Person 1234567", Adress = "Country 1", Salary = 70000},
+                new Person() {ID =8, Name = "Person 12345678", Adress = "Country 1", Salary = 80000}
+            };
+            return people;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Linq001.Models;
+
+namespace Linq001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//< ou >
+            List<Person> people = RepositoryPerson.persons();
+
+            /*foreach(Person p in people)
+            {
+                Console.WriteLine($"Person id is {p.ID}");
+            }*/
+
+            Person p = people.Where(x => x.ID == 1).First();
+            Console.WriteLine($"Person is {p.Name}");
+
+            Person p31 = people.Where(x => x.ID == 31).First();
+
+            if(p31 is null)
+            {
+                Console.WriteLine("Person not found");
+            }
+            else
+            {
+                Console.WriteLine($"Person is {p31.Name}");
+            }
+
+            List<Person> Listbelow5k = people.Where(x => x.Salary <=50000).ToList();
+
+            foreach(Person currentperson in Listbelow5k)
+            {
+                Console.WriteLine($"Person id is {currentperson.ID} and earns {currentperson.Salary}");
+            }
+        }
+    }
+}
